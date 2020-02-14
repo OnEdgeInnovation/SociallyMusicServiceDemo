7447ce660c795aa0a4db85acd19e5f5c23e15f7c
Zach McGuckin <zachmcguckin@gmail.com>
2020-02-13 19:14:26 -0500

    Initial Commit

---
+++ SociallyMusicServiceDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SociallyMusicServiceDemo
+//
+//  Created by Zach McGuckin on 2/13/20.
+//  Copyright © 2020 Zach McGuckin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
